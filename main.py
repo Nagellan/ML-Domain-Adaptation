01cd700be4069962efae1a75036b1f54ed5b9cb0
Nagellan <31626879+Nagellan@users.noreply.github.com>
2019-11-03 00:14:03 +0300

    Make accuracy 55.09%

---
+++ main.py
@@ -0,0 +1,108 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+
+TRAIN_BATCH_SIZE = 32
+TEST_BATCH_SIZE = 100
+DATA_FOLDER = "data"
+
+
+# Transformations
+train_data_transformations = transforms.Compose([
+    transforms.Resize(28),
+    transforms.Grayscale(),
+    transforms.ToTensor(),
+    transforms.Normalize((0.1307,), (0.3081,))
+])
+
+test_data_transformations = transforms.Compose([
+    transforms.ToTensor(),
+    # transforms.ColorJitter(brightness=0, contrast=0,
+    transforms.Normalize((0.1307,), (0.3081,))
+])
+
+# Data Source
+svhn_train = datasets.SVHN(DATA_FOLDER, download=True, split="train", transform=train_data_transformations)
+svhn_test = datasets.SVHN(DATA_FOLDER, download=True, split="test", transform=test_data_transformations)
+mnist_test = datasets.MNIST(DATA_FOLDER, download=True, train=False, transform=test_data_transformations)
+
+# Data loaders
+train_svhn_loader = DataLoader(svhn_train, batch_size=TRAIN_BATCH_SIZE, shuffle=True)
+test_svhn_loader = DataLoader(svhn_test, batch_size=TEST_BATCH_SIZE, shuffle=True)
+test_mnist_loader = DataLoader(mnist_test, batch_size=TEST_BATCH_SIZE, shuffle=True)
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, 10)
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, 320)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x)
+
+
+use_cuda = torch.cuda.is_available()
+device = torch.device("cuda" if use_cuda else "cpu")
+model_cnn = Net().to(device)
+
+
+def train(model, device, train_loader, optimizer, epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                       100. * batch_idx / len(train_loader), loss.item()))
+
+
+def test(model, device, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+
+
+epochs = 10
+lr = 0.01
+momentum = 0.5
+log_interval = 700
+
+model = model_cnn
+optimizer = optim.SGD(model.parameters(), lr=lr, momentum=momentum)
+
+for epoch in range(1, epochs + 1):
+    train(model, device, train_svhn_loader, optimizer, epoch)
+    test(model, device, test_mnist_loader)
+
+    torch.save(model.state_dict(), "mnist_inno.pt")
